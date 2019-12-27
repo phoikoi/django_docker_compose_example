93666a0a343b40abbb6a449345428ec38c99dbf9
Peter Hull <design.services@oikoi.com>
2019-12-27 15:21:47 -0800

    Correct wsgi app name

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd app
 ./manage.py migrate --noinput
-exec gunicorn app.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
